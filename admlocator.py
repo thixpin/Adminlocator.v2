087027ef09c0622ddb0bcebe695773bb38a9503e
thixpin <thixpin@gmail.com>
2014-01-24 20:49:31 +0630

    Update admlocator.py

--- admlocator.py
+++ admlocator.py
@@ -3,21 +3,23 @@
 import sys
 import os
 import httplib
+import socket
 
 #General Stuffs start
 def banner():
         print '''
-                                                                                         
-                .oo      8          o       o                            o               
-               .P 8      8                  8                            8               
-              .P  8 .oPYo8 ooYoYo. o8 odYo. 8     .oPYo. .oPYo. .oPYo.  o8P .oPYo. oPYo. 
-             oPooo8 8    8 8' 8  8  8 8' `8 8     8    8 8    ' .oooo8   8  8    8 8  `' 
-            .P    8 8    8 8  8  8  8 8   8 8     8    8 8    . 8    8   8  8    8 8     
-           .P     8 `YooP' 8  8  8  8 8   8 8oooo `YooP' `YooP' `YooP8   8  `YooP' 8     
-           ..:::::..:.....:..:..:..:....::........:.....::.....::.....:::..::.....:..::::
-           ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-           ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-                                   ver.2 (-) Mr.Geek (-) 4Sectors
+                                                                                        
+       .oo      8          o       o                            o        
+      .P 8      8                  8                            8         
+     .P  8 .oPYo8 ooYoYo. o8 odYo. 8     .oPYo. .oPYo. .oPYo.  o8P .oPYo. oPYo
+    oPooo8 8    8 8' 8  8  8 8' `8 8     8    8 8    ' .oooo8   8  8    8 8  `
+   .P    8 8    8 8  8  8  8 8   8 8     8    8 8    . 8    8   8  8    8 8   
+  .P     8 `YooP' 8  8  8  8 8   8 8oooo `YooP' `YooP' `YooP8   8  `YooP' 8   
+  ..:::::..:.....:..:..:..:....::........:.....::.....::.....:::..::.....:..::
+  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+                        ver.2.1 (-) Mr.Geek (-) 4Sectors
+                            Recoded by thixpin
         '''
 def usage():
         print '''
@@ -25,18 +27,18 @@ def usage():
                 $ ./adminlocator.py -u <url>
                 <url> format ==> http://www.google.com  ,etc.
         '''
+    
 
 def clear():
         if os.name == 'posix':
                 cmd = 'clear'
         else:
-                cmd = 'cls'
+                cmd = 'cls && color 0A'
         os.system(cmd)
 
 def MSG(msg):
         if msg == 'adminrun':
                 print('[*] Starting Admin Locator ...')
-                print('[-] Brute forcing admin paths (-)> %s'%(sys.argv[2]))
         elif msg == 'found':
                 print('[!] Found Admin page @> ')
         elif msg == 'redirect':
@@ -49,7 +51,13 @@ def MSG(msg):
 #General Stuffs end
 #Classes start
 class wordlist:
-        adminwl = 'admins.txt'
+        adminwl = 'all.txt'
+        adminwl1 = 'php.txt'
+        adminwl2 = 'asp.txt'
+        adminwl3 = 'cfm.txt'
+        adminwl4 = 'js.txt'
+        adminwl5 = 'cgi.txt'
+        adminwl6 = 'brf.txt'
 
 class Bruter(object):
         def __init__(self,url,wordlist):
@@ -74,12 +82,14 @@ class Bruter(object):
                         for path in self.filecontent:
                            try:
                                 self.path = "/"+path
-                                conn = httplib.HTTPConnection(self.url)
+                                print ("\t [#] Checking " + self.url + self.path )
+                                conn = httplib.HTTPConnection(self.url) 
                                 conn.request("GET",self.path)
                                 resp = conn.getresponse()
                                 if resp.status == 200:
                                         MSG('found')
                                         print '[-]> %s%s'%(self.url,self.path)
+                                        raw_input("Press enter to continue scanning.\n")
                                         self.count += 1
                                         self.foundpaths.append('%s%s'%(self.url,self.path))
                                 elif resp.status == 302:
@@ -91,7 +101,7 @@ class Bruter(object):
                                         pass
                            except KeyboardInterrupt:
                                    print '[~] KeyboardInterrupt'
-                                   print '[~] Exiting ...'
+                                   print '[~] Exiting ... \n'
                                    self.printf()
                                    sys.exit(0)
                                    
@@ -99,21 +109,61 @@ class Bruter(object):
                                 print('[*] General Error occurs ...')
                                 print('[-] Terminating ...')
                                 sys.exit(0)
-                
+        
         def run(self):
                 MSG('adminrun')
 
         def printf(self):
-                print '=========================================='
-                print '[*] Total Found Admin page or pages (-)> %s'%(self.count)
-                print self.foundpaths                
+                clear()
+                banner()
+                print '\n \n \t Scan Conpleted for Target: ' + self.url + ' \n '
+                print '\t [*] Total Found Admin page or pages (-)> %s'%(self.count)
+                print self.foundpaths
+                print '\n \n'
                                 
 #Classes end
-#Functions start        
-def AdminLocator(domain):
+#Functions start
+def main():
+        try:
+                site = raw_input("Web Site for Scan?: ")
+                site = site.replace("http://","")
+                print ("\tChecking website " + site + "...")
+                conn = httplib.HTTPConnection(site)
+                conn.connect()
+                print "\t[$] Yes... Server is Online."
+        except (httplib.HTTPResponse, socket.error) as Exit:
+                raw_input("\t [!] Oops Error occured, Server offline or invalid URL")
+                exit()
+        print "Enter site source code:"
+        print "1 PHP"
+        print "2 ASP"
+        print "3 CFM"
+        print "4 JS"
+        print "5 CGI"
+        print "6 BRF"
+        print "7 All"
+        print "\nPress 1 and 'Enter key' for Select PHP\n"
+        ec=input("> ")
+        AdminLocator(site,ec)
+        
+def AdminLocator(domain,ec):
         '''Admin Finder function'''
         url = domain
-        b = Bruter(url,wordlist.adminwl)
+        code = ec
+        if code==1:
+                b = Bruter(url,wordlist.adminwl1)
+        if code==2:
+                b = Bruter(url,wordlist.adminwl2)
+        if code==3:
+                b = Bruter(url,wordlist.adminwl3)
+        if code==4:
+                b = Bruter(url,wordlist.adminwl4)
+        if code==5:
+                b = Bruter(url,wordlist.adminwl5)
+        if code==6:
+                b = Bruter(url,wordlist.adminwl6)
+        if code==7:
+                b = Bruter(url,wordlist.adminwl)
         b.run()
         b.brute()
         b.printf()
@@ -121,15 +171,6 @@ def AdminLocator(domain):
 
 #Functions end
 
-def main():
-        '''Main function'''
-        if len(sys.argv) == 3 and sys.argv[1] == '-u' and sys.argv[2].startswith('http://'):
-                AdminLocator(sys.argv[2])
-
-        else:
-                usage()
-
-#Py_ver = sys.version.split()[0]#testing...
 
 if __name__ == "__main__":
         clear()
